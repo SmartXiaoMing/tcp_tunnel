b4ece5610230ea45caac848dca971ab791bd9379
SmartXiaoMing <95813422@qq.com>
2016-09-11 20:35:13 +0800

    fix code style
    N/A
    
    fix code style

--- src/tcp_server.cpp
+++ src/tcp_server.cpp
@@ -230,18 +230,15 @@ TcpServer::handleTunnelClient(uint32_t events, int eventFd) {
     return false;
   }
   if ((events & EPOLLRDHUP) || (events & EPOLLERR)) {
-    log_error << "fd: " << eventFd << ", events: " << events;
     cleanUpTunnelClient(eventFd);
     return true;
   }
   if ((events & EPOLLIN) == 0) {
-    log_error << "fd: " << eventFd << ", events: " << events;
     return true;
   }
   char buf[BUFFER_SIZE];
   int len = recv(eventFd, buf, BUFFER_SIZE, 0);
   if (len <= 0) {
-    log_error << "fd: " << eventFd << ", len: " << len;
     cleanUpTunnelClient(eventFd);
     return true;
   }
